8e9e187af11645fa953ac7db8e5a58a4bb291b98
yanceyy <yanceyy@users.noreply.github.com>
2024-04-11 18:47:16 +1000

    feat: add optional copy button to the code block

--- src/code.tsx
+++ src/code.tsx
@@ -1,35 +1,37 @@
 import { getBlockTitle } from "notion-utils";
-import React, { useEffect, useState } from "react";
+import { IoMdCopy } from "react-icons/io";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { cs, useNotionContext, Text } from "react-notion-x";
 import { codeToHtml } from "shiki";
-import { BundledTheme } from "shiki/themes";
-
 import styles from "./code.module.css";
 
 import type { CodeBlock } from "notion-types";
+import type { BundledTheme } from "shiki/themes";
 
 export const Code: React.FC<{
   block: CodeBlock;
   defaultLanguage?: string;
   className?: string;
-  captionClassName?: string;
+  showCopy?: boolean;
   themes?: {
     light: BundledTheme;
     dark: BundledTheme;
   };
 }> = ({
   block,
+  className,
   defaultLanguage = "typescript",
   themes = {
     light: "catppuccin-latte",
     dark: "dracula"
   },
-  className,
-  captionClassName
+  showCopy = true
 }) => {
   const { recordMap } = useNotionContext();
+  const [isCopied, setIsCopied] = useState(false);
   const content = getBlockTitle(block, recordMap);
   const [code, setCode] = useState<string | undefined>(undefined);
+  const timer = useRef<null | number>(null);
 
   const language = (
     block.properties?.language?.[0]?.[0] || defaultLanguage
@@ -47,15 +49,38 @@ export const Code: React.FC<{
     content && renderCodeToHtml();
   }, [content, language, themes]);
 
+  const clickCopy = useCallback(() => {
+    navigator.clipboard.writeText(content).then(() => {
+      setIsCopied(true);
+      if (timer.current) {
+        clearTimeout(timer.current);
+        timer.current = null;
+      }
+      timer.current = setTimeout(() => {
+        setIsCopied(false);
+        timer.current = null;
+      }, 1000);
+    });
+  }, [content]);
+
   return (
     <figure className={cs(styles.codeBlock, className)}>
+      {showCopy ? (
+        <button
+          onClick={clickCopy}
+          className={styles.codeCopyButton}
+        >
+          <IoMdCopy />
+          {isCopied ? "Copied" : "Copy"}
+        </button>
+      ) : null}
       {code == undefined ? (
         <pre>{content}</pre>
       ) : (
         <div dangerouslySetInnerHTML={{ __html: code }} />
       )}
       {caption && (
-        <figcaption className={cs(styles.codeCaption, captionClassName)}>
+        <figcaption className={styles.codeCaption}>
           <Text value={caption} block={block} />
         </figcaption>
       )}
